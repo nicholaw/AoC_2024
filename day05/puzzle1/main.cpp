98766eb02aba80d0dca1cc271afd9c31da1cd795
WARNER Nicholas <nicholas.warner@biomerieux.com>
2024-12-31 14:09:11 -0700

    Implemented methods for cheking updates are legal and summing modes of legal updates; implementation functions as expected but has O(N^2) complexity.

--- day05/puzzle1/main.cpp
+++ day05/puzzle1/main.cpp
@@ -1,27 +1,30 @@
 #include "utils.h"
 
 /**
- * Returns true if the provide key in not contained in the provided ruleset or if a key-value is present in the 
- * provided search vector and false otherwise.
+ * Returns false if any prerequisites for the provided key present in the provided update vector are present but appear 
+ * after the provided key index and true otherwise.
  */
-bool prereqPresent(std::vector<int> searchvector, int end, std::map<int, std::set<int>> ruleset, int key) {
-    if(!util::containsKey(ruleset, key)) {
-        return true;
-    }
-    for(int i = 0; i < end; i++) {
-        if(util::contains(ruleset.at(key), searchvector.at(i))) {
-           return true; 
+bool prereqPresentAndOrdered(std::vector<int> searchVector, int keyIndex, std::map<int, std::set<int>> ruleset) {
+    if(util::containsKey(ruleset, searchVector.at(keyIndex))) {
+        for(int i = 0; i < searchVector.size(); i++) {
+            for(int j : ruleset.at(searchVector.at(keyIndex))) {
+                if(searchVector.at(i) == j) {
+                    if(i >= keyIndex) {
+                        return false;
+                    }
+                }
+            }
         }
     }
-    return false;
-}//prereqPresent
+    return true;
+}//prereqPresentAndOrdered
 
 /**
  * Returns true if the provided update is legal per the provided ruleset and false otherwise.
  */
 bool isLegal(std::vector<int> update, std::map<int, std::set<int>> ruleset) {
     for(int i = 0; i < update.size(); i++) {
-        if(!prereqPresent(update, i, ruleset, update.at(i))) {
+        if(!(prereqPresentAndOrdered(update, i, ruleset))) {
             return false;
         }
     }
@@ -33,14 +36,11 @@ bool isLegal(std::vector<int> update, std::map<int, std::set<int>> ruleset) {
  */
 int countModes(std::vector<std::vector<int>> updates, std::map<int, std::set<int>> ruleset) {
     int sum = 0;
-    std::vector<std::vector<int>> legalUpdates;
     for(std::vector<int> v : updates) {
         if(isLegal(v, ruleset)) {
-            legalUpdates.push_back(v);
             sum += (v.at(v.size() / 2));
         }
     }
-    util::writeUpdates(legalUpdates, "output_updates.txt");
     return sum;
 }//coutModes
 
