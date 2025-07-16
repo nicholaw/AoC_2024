82bdc0d1592a3fc979939c2fdc0bd7bf112b0f14
WARNER Nicholas <nicholas.warner@biomerieux.com>
2025-07-16 12:11:12 -0600

    implemented method for to insert products into temporary array and then search that array for anything that matches the target calibration

--- day07/puzzle1/main.cpp
+++ day07/puzzle1/main.cpp
@@ -5,99 +5,78 @@
 #include <string>
 #include <vector>
 
-const int NUM_OPERATORS = 2;
+double calibrationTotal = 0;
 
-/**
- * Build map of calibrations from the provided input file.
- */
-std::map<int, std::vector<int>> readInput(std::string input) {
+void readInput(std::string input) {
     std::ifstream inputFile;
-    std::map<int, std::vector<int>> calibrations;
     try {
         inputFile.open(input);
     } catch(std::exception e) {
         std::cout << "Cannot find file \'" << input << "\'\n";
     }
+
     std::string line;
     std::string str;
-    int key;
-    bool skip;
+    bool skipSpace;
+    double target;
+    std::vector<double>* operands;
+    std::vector<double>* temp;
+
+    //Loop through input file and determine if target calibration can be achieved with given numbers from input file.
     while(std::getline(inputFile, line)) {
-        std::vector<int> operands = {};
-        skip = true;
         str = "";
-        for(char c : line) {
-            if(c == ' ') {
-                if(skip) {
-                    skip = false;
-                } else {
-                    operands.push_back(std::stoi(str));
+        skipSpace = true;
+        operands = new std::vector<double>();
+
+        //Loop through current line from input file.
+        for(int i = 0; i < line.size(); i++) {
+            temp = new std::vector<double>();
+            switch(line.at(i)) {
+                case ' ':
+                    if(!skipSpace) {
+                        if(operands->size() <= 0) {
+                            operands->push_back(std::stod(str));
+                        } else {
+                            double next = std::stod(str);
+                            for(int j = 0; j < operands->size(); j++) {
+                                temp->push_back(operands->at(j) + next);
+                                temp->push_back(operands->at(j) * next);
+                            }
+                            operands = temp;
+                        }
+                        str = "";
+                    } else {
+                        skipSpace = false;
+                    }
+                    break;
+                case ':':
+                    target = std::stod(str);
                     str = "";
-                }
-            } else if(c == ':') {
-                key = std::stoi(str);
-                str = "";
-            } else {
-                str += c;
+                    break;
+                default:
+                    str += line.at(i);
+            }//switch
+        }//for
+        double next = std::stod(str);
+        temp = new std::vector<double>();
+        for(int j = 0; j < operands->size(); j++) {
+            temp->push_back(operands->at(j) + next);
+            temp->push_back(operands->at(j) * next);
+        }
+        operands = temp;
+
+        //If operands contains target, add target to calibrationTotal
+        for(int i = 0; i < operands->size(); i++) {
+            if(target == operands->at(i)) {
+                calibrationTotal += target;
+                break;
             }
         }
-        operands.push_back(std::stoi(str));
-        calibrations.insert({key, operands});
-    }
-    return calibrations;
-};
-
-/**
- * Writes the provided map to the provided destination.
- */
-void writeOutput(std::map<int, std::vector<int>> calibrations, std::string destination) {
-    std::ofstream outputFile;
-    outputFile.open(destination);
-    for(std::pair p : calibrations) {
-        outputFile << p.first << " = ";
-        for(int i = 0; i < p.second.size(); i++) {
-            if(i == p.second.size() - 1) {
-                outputFile << calibrations.at(p.first).at(i) << "\n";
-            } else {
-                outputFile << calibrations.at(p.first).at(i) << " ";
-            }
-        }
-    }
-};
-
-/**
- * Retunrs true if the provided int can be reached with a permutation of operators but witout 
- * changing the order of the provided operands.  
- */
-bool isSolvable(int target, std::vector<int> operands) {
-    int product = operands.at(0);
-    int numMultiples = operands.size() - 1;
-    for(int i = 0; i < (NUM_OPERATORS^(operands.size() - 1)); i++) {
-        for(int j = 1; j < operands.size(); j++) {
-            if() {
-
-            } else {
-
-            }
-        }
-    }
-    return false;
-}
-
-/**
- * Returns the sum of the provided calibrations which are solvable.
- */
-int sumSolvable(std::map<int, std::vector<int>> calibrations) {
-    int sum = 0;
-    for(std::pair p : calibrations) {
-        if(isSolvable(p.first, p.second)) {
-            sum += p.first;
-        }
-    }
-    return sum;
-};//sumSolvable
+    }//while
+}//readInput
 
 int main() {
-    std::cout << sumSolvable(readInput("input.txt")) << "\n";
+    readInput("input.txt");
+    std::cout << calibrationTotal;
     return 0;
 }//main
