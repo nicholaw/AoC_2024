009e576cc03e129a1af2cfb5d472e9ca58d1db8c
WARNER Nicholas <nicholas.warner@biomerieux.com>
2025-07-18 11:17:55 -0600

    Refactored code (again) into modular functions to increase readability; code outputs as expected for test input.

--- day07/puzzle2/main.cpp
+++ day07/puzzle2/main.cpp
@@ -1,25 +1,116 @@
 #include <exception>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
-double calibrationTotal = 0;
+/**
+ * Returns the concatination of the provided numbers.
+ * For example, provided 12 and 34, returns 1234
+ */
+uint64_t concatNum(uint64_t a, uint64_t b) {
+    return (std::stoi(std::to_string(a) + std::to_string(b)));
+}//concatNum
 
-void readInput(std::string input) {
-    std::ifstream inputFile;
-    try {
-        inputFile.open(input);
-    } catch(std::exception e) {
-        std::cout << "Cannot find file \'" << input << "\'\n";
+/**
+ * Returns true if the provided number is present in the provided set and false otherwise.
+ */
+bool contains(uint64_t target, std::vector<uint64_t>* set) {
+    for(int i = 0; i < set->size(); i++) {
+        if(target == set->at(i)) {
+            return true;
+        }
+    }
+    return false;
+}//contains
+
+/**
+ * Returns the target number if the provided calibration can be solved with legal operators and 0 otherwise.
+ */
+uint64_t solveCalibration(uint64_t target, std::vector<uint64_t>* operands) {
+    std::vector<uint64_t>* products = new std::vector<uint64_t>();
+    std::vector<uint64_t>* temp;
+    
+    for(int i = 0; i < operands->size(); i++) {
+        uint64_t next = operands->at(i);
+        if(i == 0) {
+            products->push_back(next);
+        } else {
+            temp = new std::vector<uint64_t>();
+            for(int j = 0; j < products->size(); j++) {
+                temp->push_back(products->at(j) + next);
+                temp->push_back(products->at(j) * next);
+                temp->push_back(concatNum(products->at(j), next));
+            }
+            products = temp;
+        }
     }
 
-    //Loop through input file and determine if target calibration can be achieved with given numbers from input file.
-    while(std::getline(inputFile, line)) {
-    }//while
-}//readInput
+    if(contains(target, products)) {
+        return target;
+    } else {
+        return 0;
+    }
+}//solveCalibration
+
+/**
+ * Parses the provided line into a target calibration and list of operands; returns the target
+ * calibration if the line can be solved and zero otherwise.
+ */
+uint64_t parseLine (std::string line) {
+    uint64_t target = 0;
+    std::string str = "";
+    std::vector<uint64_t>* operands = new std::vector<uint64_t>();
+    bool skip = true;
+    for(int i = 0; i < line.size(); i++) {
+        switch(line.at(i)) {
+            case ':':
+                target = std::stoi(str);
+                str = "";
+                break;
+            case ' ':
+                if(skip) {
+                    skip = false;
+                } else {
+                    operands->push_back(std::stoi(str));
+                    str = "";
+                }
+                break;
+            default:
+                str += line.at(i);
+                break;
+        }
+    }
+    operands->push_back(std::stoi(str));
+
+    return solveCalibration(target, operands);
+}//parseLine
+
+/**
+ * Reads the provided input file and returns sum of legal calibrations.
+ */
+uint64_t run(std::string filename) {
+    //Open input file; catch FileNotFound Exception
+    uint64_t calibrationTotal = 0;
+    std::ifstream input;
+    try {
+        input.open(filename);
+    } catch(std::exception e) {
+        std::cout << "Could not find file \"" << filename << "\"\n";
+        return 0;
+    }
+
+    //Read input file
+    std::string line;
+    while(std::getline(input, line)) {
+        calibrationTotal += parseLine(line);
+    }
+
+    return calibrationTotal;
+}//run
 
 int main() {
+    std::cout << std::to_string(run("input.txt"));
     return 0;
 }//main
